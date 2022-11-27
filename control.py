65adc2ef885e691a6ad1e9c6b666575e7fc88985
Wilbster <95505563+Wilbster@users.noreply.github.com>
2022-11-27 10:16:07 -0800

    Fix glitch with valiate_move

--- control.py
+++ control.py
@@ -9,10 +9,7 @@ A01331142
 def get_user_choice():
 
     print('Choose a direction in which to explore.')
-    print('1: North')
-    print('2: South')
-    print('3: East')
-    print('4: West')
+    print('1: North --- 2: South --- 3: East --- 4: West')
     choice = input()
     if choice == '1':
         return 'North'
@@ -30,17 +27,17 @@ def get_user_choice():
 def validate_move(board, character, direction):
     coordinates = (character['X-coordinate'], character['Y-coordinate'])
     if direction == 'North':
-        new_coord = coordinates[0] - 1
-        new_coordinates = (new_coord, coordinates[1])
-    elif direction == 'South':
-        new_coord = coordinates[0] + 1
-        new_coordinates = (new_coord, coordinates[1])
-    elif direction == 'West':
         new_coord = coordinates[1] - 1
         new_coordinates = (coordinates[0], new_coord)
-    elif direction == 'East':
+    elif direction == 'South':
         new_coord = coordinates[1] + 1
         new_coordinates = (coordinates[0], new_coord)
+    elif direction == 'West':
+        new_coord = coordinates[0] - 1
+        new_coordinates = (new_coord, coordinates[1])
+    elif direction == 'East':
+        new_coord = coordinates[0] + 1
+        new_coordinates = (new_coord, coordinates[1])
     if new_coordinates in board:
         return True
     else:
