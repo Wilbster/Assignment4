21c211d811f8a5735fae7991f9121ff794a66ad2
Wilbster <95505563+Wilbster@users.noreply.github.com>
2022-11-27 00:19:27 -0800

    Add character module

--- character.py
+++ character.py
@@ -9,12 +9,16 @@ A01331142
 def make_character(name):
     character = {"Name": name, "Level": 1, "X-coordinate": 0, "Y-coordinate": 0, "Current HP": 5, "Max HP": 5,
                  "Experience": 0}
+    print(f"Welcome to the realm of Thompsons, Monster Hunter {name}. Our lord and soverign, Supereme Ruler")
+    print("Christopher, rules with an iron fist.")
+    print('Enter anything to continue.')
+    input()
     return character
 
 
 def choose_name():
 
-    print('Choose you name for this quest:')
+    print('Choose your name for this quest, Witcher.')
     name = input()
     return name
 
