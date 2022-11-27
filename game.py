08b6a3009d0130bbb16ae2bde7f81f84449bcca6
Wilbster <95505563+Wilbster@users.noreply.github.com>
2022-11-27 00:19:50 -0800

    Modify game module

--- game.py
+++ game.py
@@ -1,31 +1,46 @@
 """
+Olga Zimina
+A01307815
 Wilber Lin
 A01331142
 """
+from board import make_board
+from character import make_character, choose_name
+from describe import describe_current_location
+from control import get_user_choice, validate_move, move_character
+
+
 
 def game(): # called from main
     rows = 10
     columns = 10
     board = make_board(rows, columns)
-    character = make_character("Player name")
+    player_name = choose_name()
+    character = make_character(player_name)
     achieved_goal = False
     while not achieved_goal:
-        // Tell the user where they are
+        #Tell the user where they are
         describe_current_location(board, character)
-        direction = get_user_choice( )
+        direction = get_user_choice()
         valid_move = validate_move(board, character, direction)
+        print((character['X-coordinate'], character['Y-coordinate']))
         if valid_move:
-            move_character(character)
+            move_character(character, direction)
             describe_current_location(board, character)
+            """
             there_is_a_challenge = check_for_challenges()
             if there_is_a_challenge:
                 execute_challenge_protocol(character)
                 if character_has_leveled():
                     execute_glow_up_protocol()
                 achieved_goal = check_if_goal_attained(board, character)
+            """
         else:
-            // Tell the user they can’t go in that direction
-    // Print end of game stuff like congratulations or sorry you died
+            print('That is outside the boundaries of our realm!')
+            print('Enter anything to continue.')
+            input()
+            # Tell the user they   can’t go in that direction
+    # Print end of game stuff like congratulations or sorry you died
 
 
 def main():
