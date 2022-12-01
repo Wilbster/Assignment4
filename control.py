abd371a756296bebb55a88e603fe72b8ae428b29
Olga Zimina <olga.vic.zimina@gmail.com>
2022-11-30 18:47:06 -0800

    Update validate_character_move

--- control.py
+++ control.py
@@ -4,9 +4,10 @@ A01307815
 Wilber Lin
 A01331142
 """
+from exceptions import OutOfTheRealmError
 
 
-def get_user_choice():
+def get_player_choice_v1():
 
     print('Choose a direction in which to explore.')
     print('1: North --- 2: South --- 3: East --- 4: West')
@@ -21,37 +22,64 @@ def get_user_choice():
         return 'West'
     else:
         print('Invalid')
-        return get_user_choice()
+        return get_player_choice()
+
+def show_available_options(options):
+    for number, option in enumerate(options, 1):
+        print(f'{number} - {option}')
+
+def get_player_choice(situation, options):
+    print(situation)
+    show_available_options(options)
+    choice = int(input("Enter your choice "))
+    for number, option in enumerate(options, 1):
+        if choice == number:
+            print(f"You have selected {option}")
+            return option
+    print("You must be tired from your long journeys. Take another shot")
+    return get_player_choice(situation, options)
 
 
-def validate_move(board, character, direction):
-    coordinates = (character['X-coordinate'], character['Y-coordinate'])
+# def validate_move(board, character, direction):
+#     coordinates = (character['X-coordinate'], character['Y-coordinate'])
+#     if direction == 'North':
+#         new_coord = coordinates[1] - 1
+#         new_coordinates = (coordinates[0], new_coord)
+#     elif direction == 'South':
+#         new_coord = coordinates[1] + 1
+#         new_coordinates = (coordinates[0], new_coord)
+#     elif direction == 'West':
+#         new_coord = coordinates[0] - 1
+#         new_coordinates = (new_coord, coordinates[1])
+#     elif direction == 'East':
+#         new_coord = coordinates[0] + 1
+#         new_coordinates = (new_coord, coordinates[1])
+#     if new_coordinates in board:
+#         return True
+#     else:
+#         return False
+
+
+def define_updated_location(character, direction):
+    current_location = character.get_current_location()
     if direction == 'North':
-        new_coord = coordinates[1] - 1
-        new_coordinates = (coordinates[0], new_coord)
+        current_location[1] - 1
     elif direction == 'South':
-        new_coord = coordinates[1] + 1
-        new_coordinates = (coordinates[0], new_coord)
+        current_location[1] + 1
     elif direction == 'West':
-        new_coord = coordinates[0] - 1
-        new_coordinates = (new_coord, coordinates[1])
+        current_location[0] - 1
     elif direction == 'East':
-        new_coord = coordinates[0] + 1
-        new_coordinates = (new_coord, coordinates[1])
-    if new_coordinates in board:
-        return True
+        current_location[0] + 1
+    return current_location
+
+
+def validate_character_move(character, direction):
+    updated_location = define_updated_location(character, direction)
+    if updated_location[0] < 0 or updated_location[1] < 0 or updated_location[0] > 9 or updated_location[1] > 9:
+        raise OutOfTheRealmError()
     else:
-        return False
+        return True
 
-def move_character(character, direction):
-    if direction == 'North':
-        character['Y-coordinate'] -= 1
-    elif direction == 'South':
-        character['Y-coordinate'] += 1
-    elif direction == 'West':
-        character['X-coordinate'] -= 1
-    elif direction == 'East':
-        character['X-coordinate'] += 1
 
 def main():
     pass
