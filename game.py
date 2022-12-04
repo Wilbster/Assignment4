600dd1de8da90d099e11ad52065cf4ef976ae5f3
Olga Zimina <olga.vic.zimina@gmail.com>
2022-12-04 15:14:56 -0800

    Update board and character setup for game()

--- game.py
+++ game.py
@@ -4,22 +4,19 @@ A01307815
 Wilber Lin
 A01331142
 """
-from board import make_board, get_land_layout, get_map_legend, draw_map, set_enemies, generate_description
+from board import get_map_legend, draw_map
 from character import Character, character_should_level, level_up
-from settings import make_character, choose_name
+from settings import make_character, display_narrator_text, set_up_board
 from describe import describe_current_location
 from control import validate_move, get_player_choice
 from challenge import check_for_enemies, execute_combat_protocol
 
 
 def game(): # called from main
-    layout = get_land_layout("Verden")
-    board = make_board(layout)
-    generate_description(board)
-    set_enemies(board)
+    display_narrator_text("start_page.txt")
     map_legend = get_map_legend("Verden")
-    character = Character("OZ")
-    draw_map(board, map_legend, character.get_current_location())
+    board = set_up_board("Verden")
+    character = make_character()
     achieved_goal = False
     while not achieved_goal and character.get_current_hp() > 0:
         #Tell the user where they are
@@ -30,11 +27,10 @@ def game(): # called from main
         valid_move = validate_move(character, direction)
         if valid_move:
             character.move_to(direction)
-            # move_character(character, direction)
             print(character.get_current_location())
             character_location = character.get_current_location()
             draw_map(board, map_legend, character_location)
-            #describe_current_location(board, character)
+            board[character_location].describe_location()
             enemy_fight = check_for_enemies(character, board)
             if enemy_fight:
                 execute_combat_protocol(character, board)
