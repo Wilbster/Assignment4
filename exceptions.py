c642a2e823bf09b4e9534a1d9409f5c5addc1420
Olga Zimina <olga.vic.zimina@gmail.com>
2022-12-04 22:11:30 -0800

    Remove unused file

--- exceptions.py
+++
@@ -1,6 +0,0 @@
-class OutOfTheRealmError(Exception):
-
-    def __init__(self, message="You have reached the world's edge. "
-                               "None but dragons play past here. You should turn back."):
-        self.message = message
-        super().__init__(self.message)
