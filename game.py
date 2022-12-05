9244577ccebc79244a421a0db355791a80001c7e
Olga Zimina <olga.vic.zimina@gmail.com>
2022-12-04 16:55:19 -0800

    Revert "Fix coordinates to be a tuple"
    This reverts commit 6b34d23346b34fef337974b67953c54191de6cb9.

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ A01331142
 """
 from board import get_map_legend, draw_map
 from character import Character, character_should_level, level_up
-from settings import make_character, display_narrator_text, set_up_board, pause
+from settings import make_character, display_narrator_text, set_up_board
 from describe import describe_current_location
 from control import validate_move, get_player_choice
 from challenge import check_for_enemies, execute_combat_protocol
@@ -14,7 +14,6 @@ from challenge import check_for_enemies, execute_combat_protocol
 
 def game(): # called from main
     display_narrator_text("start_page.txt")
-    pause()
     map_legend = get_map_legend("Verden")
     board = set_up_board("Verden")
     character = make_character()
@@ -31,7 +30,7 @@ def game(): # called from main
             print(character.get_current_location())
             character_location = character.get_current_location()
             draw_map(board, map_legend, character_location)
-            board[(character_location[0], character_location[1])].describe_location()
+            board[character_location].describe_location()
             enemy_fight = check_for_enemies(character, board)
             if enemy_fight:
                 execute_combat_protocol(character, board)
